85d9bd9c8a28ecce0a0cca66b13c9cf3e3bc09e1
J. Christian Romero <jcromerohdz@gmail.com>
2022-04-01 08:35:09 -0700

    Initial commit with react bootstrap and axios packeges

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Container } from "react-bootstrap"
+
+function App() {
+
+  return (
+		<>
+			<header className="py-5">
+				<h1>Drinks Browser</h1>
+			</header>
+
+			<Container className='mt-5'>
+				1
+			</Container>
+		</>
+  )
+}
+
+export default App
